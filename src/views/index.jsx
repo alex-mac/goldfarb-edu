b94a633603b0eba516d4560135ccec22cd5b3f42
PalomaHennessy <palomahennessy@gmail.com>
2016-05-25 22:25:18 -0700

    updated styling

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -4,7 +4,28 @@ module.exports=React.createClass({
 	render: function(){
 		return (
 				<div>
-					<h3>HELLO ARE YOU WORKING?!!</h3>
+					<nav className="navbar navbar-fixed-top">
+					  <div className="container-fluid">
+					    <div className="navbar-header">
+					      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a className="navbar-brand" href="index.html"><img src="images/editedlogo.png" id="logo" height="75px" width="300px"></a>
+					      </div>
+
+					      <div className="collapse navbar-collapse" id="myNavbar">
+					        <ul className="nav navbar-nav navbar-right">      
+					            <li><a href="index.html">Home</a></li>
+					            <li><a href="services.html">Services</a></li>
+					            <li><a href="infobank.html">InfoBank</a></li>
+					            <li><a href="about.html">About</a></li>
+					            <li><a href="contactus.html">Contact Us</a></li>
+					        </ul>
+					      </div>
+					   </div> 
+					  </nav> 
 				</div>	
 		);
 	}
