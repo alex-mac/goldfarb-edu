00e438a5e5fa8991be809f9bdbbab38a88293ae6
alex-mac <meet.alexmac@gmail.com>
2016-06-04 15:01:45 -0700

    successful filter subtopics

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,11 +3,14 @@ console.log('controllers are a go');
 angular.module('GoldfarbCtrls', [])
 .controller('DataCtrl', ['$scope', 'DataFactory', 'SubtopicsFactory', '$filter', function ($scope, DataFactory,  SubtopicsFactory, $filter) {
 
+  $scope.data;
   DataFactory.query(function success(data) {
-    $scope.data = data;
+    $scope.data = data;  console.log($scope.data);
+
   }, function error(err) {
     console.log(err)
   });
+
 
   $scope.activate = function(item) {
     item.selected ? item.selected = false : item.selected = true;
@@ -18,19 +21,17 @@ angular.module('GoldfarbCtrls', [])
        selected: true
      }, true);
 
-    for (var i = 0; i < x.length; i++) {
-      x[i] = x[i].id
-    }
-     // console.log(x[0].id);
-     console.log(x)
+    // for (var i = 0; i < x.length; i++) {
+    //   x[i] = x[i].id
+    // }
+
+    $scope.subtopicData = x;
+     console.log($scope.subtopicData[0]);
+      // .subtopics[0].subtopic);
      $scope.topics = false;
      $scope.subtopics = true;
 
-     SubtopicsFactory.get({topicId: x}, function success(subTopicsData) {
-      $scope.subbyData = subTopicsData;
-     }, function error(err) {
-      console.log(err)
-     });
+     
    }
   
 }]);
